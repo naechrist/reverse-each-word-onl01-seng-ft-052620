737cdc9a9a91eeef56ded84e85bbcc0d385a6437
Jenae Christensen <729jenae.christensen@gmail.com>
2020-05-29 21:11:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,6 +3,7 @@ string = "Hello there, and how are you?"
 def reverse_each_word(sentence)
   string = sentence.split(" ")
 string.map do |word|
+  new_sentence = word.reverse
   word.reverse 
 end
 end
