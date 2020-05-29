8a02535cb6e9516ff69e8617d5b1502e83e8c39c
Jenae Christensen <729jenae.christensen@gmail.com>
2020-05-29 21:18:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,3 +8,10 @@ end
 new_array
 end
 
+def reverse_each_word(sentence)
+  array = sentence.split(" ") #changed to array since it is an array
+  new_array = array.map do |word| 
+  word.reverse 
+end
+
+#use new_array to return a string version of its content
