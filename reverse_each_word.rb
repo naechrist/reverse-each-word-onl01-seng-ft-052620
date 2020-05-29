816b9911fb00bed38ab089b4a50e599ec7380f94
Jenae Christensen <729jenae.christensen@gmail.com>
2020-05-29 21:18:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,17 +1,7 @@
-string = "Hello there, and how are you?"
 
 def reverse_each_word(sentence)
-  array = sentence.split(" ")
-new_array = string.map do |word|
-  word.reverse
-end
-new_array
-end
-
-def reverse_each_word(sentence)
-  array = sentence.split(" ") #changed to array since it is an array
+  array = sentence.split(" ") 
   new_array = array.map do |word| 
   word.reverse 
 end
 
-#use new_array to return a string version of its content
